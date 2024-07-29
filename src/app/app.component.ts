6608828d1145c0bcf6b8b53da0069dd8eafd4924
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-07-29 19:45:07 -0300

    editar um heroe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Tour of Heroes';
 }
-  // Video 7
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero.model';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Wolverine',
+  }
+
+}
